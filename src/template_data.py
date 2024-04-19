6bc6757391247bc127d31033b2b5c17348c08a70
Zhicun Chen <chen.zhicu@northeastern.edu>
2024-04-19 13:45:00 -0400

    fixed typo

--- src/template_data.py
+++ src/template_data.py
@@ -19,18 +19,18 @@ class YearValue:
 
 @dataclass
 class ReportData:
-    def __init__(self, report_id: uuid.UUID, user_input: UserInput):
-        self.report_id = report_id
-        self.user_input = user_input
+    def __init__(self):
+        self.report_id = None
+        self.user_input = None
 
         self.report_title: str = ''
 
         self.maine_maximum_market_value: YearValue = None
-        self.maine_maximum_market_volumn: YearValue = None
+        self.maine_maximum_market_volume: YearValue = None
         self.na_maximum_market_value: YearValue = None
-        self.na_maximum_market_volumn: YearValue = None
-        self.maine_maximum_average_market_value_per_pound: YearValue = None
-        self.na_maximum_average_market_value_per_pound: YearValue = None
+        self.na_maximum_market_volume: YearValue = None
+        self.maine_maximum_market_value_per_pound: YearValue = None
+        self.na_maximum_market_value_per_pound: YearValue = None
         self.maine_year_to_year_market_value_delta: list[DataDesc] = None
         self.na_year_to_year_market_value_delta: list[DataDesc] = None
 
@@ -39,5 +39,5 @@ class ReportData:
         self.na_market_value_time_series_figure: str = None
         self.maine_market_value_delta_time_series_figure: str = None
         self.na_market_value_delta_time_series_figure: str = None
-        self.maine_na_market_value_compete_figure: str = None
-        self.maine_na_market_volumn_compete_figure: str = None
+        self.maine_na_market_value_competition_figure: str = None
+        self.maine_na_market_volume_competition_figure: str = None
